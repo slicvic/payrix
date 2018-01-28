a36d2d01c6255f7359a729e15b9de4632dd99264
Victor Lantigua <vmlantigua@gmail.com>
2018-01-28 11:43:01 -0500

    Add public index.php

---
+++ App/public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+error_reporting(0);
+
+define('BASEPATH', realpath(__DIR__ . '/../../') . '/');
+define('APPPATH', BASEPATH . 'App/');
+
+require_once BASEPATH . 'vendor/autoload.php';
+
+$controllerName = !empty($_GET['c']) ? strtolower(trim($_GET['c'])) : 'index';
+$actionName = !empty($_GET['a']) ? strtolower(trim($_GET['a'])) : 'index';
+$controllerClassName = "\\App\\Controllers\\" . ucfirst($controllerName) . 'Controller';
+$actionMethodName = $actionName . 'Action';
+
+if (!class_exists($controllerClassName)) {
+    http_response_code(404);
+    die('Page not found');
+}
+
+$controllerInstance = new $controllerClassName;
+
+if (!is_callable([$controllerInstance, $actionMethodName])) {
+    http_response_code(404);
+    die('Page not found');
+}
+
+try {
+    $response = (string) $controllerInstance->$actionMethodName();
+    die($response);
+} catch (\Exception $e) {
+    http_response_code(500);
+    die('Server error');
+}
