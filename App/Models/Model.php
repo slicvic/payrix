1fdebfd7326623b04242bf132067bb19449823b3
Victor Lantigua <vmlantigua@gmail.com>
2018-01-28 13:54:55 -0500

    Add base model

---
+++ App/Models/Model.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use \PDO;
+
+abstract class Model
+{
+    /**
+     * @var PDO
+     */
+    private static $db;
+
+    /**
+     * @throws \PDOException
+     */
+    public static function getDb()
+    {
+        if (null === self::$db) {
+            self::$db = new PDO(
+                'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
+                getenv('DB_USER'),
+                getenv('DB_PASS')
+            );
+
+            self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+
+        return self::$db;
+    }
+
+    /**
+     * @param PDO $db
+     */
+    public static function setDb(PDO $db)
+    {
+        self::$db = $db;
+    }
+}
